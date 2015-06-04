d3ca2b6315c0dc79a99ed8d95a746950ec8b8c8f
hxllyl <huxllyl@gmail.com>
2015-06-04 14:49:49 +0800

    Update asset_uploader.rb
    Add class method to init argument

--- app/uploaders/kindeditor/asset_uploader.rb
+++ app/uploaders/kindeditor/asset_uploader.rb
@@ -17,6 +17,10 @@ class Kindeditor::AssetUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
+  
+  def self.setup
+    yield self
+  end  
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
